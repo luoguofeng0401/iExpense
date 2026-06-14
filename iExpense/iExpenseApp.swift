1bc910536ba4eecd888fc50dd32bb11361981209
Leo <luoguofeng0401@gmail.com>
2026-06-14 21:17:32 +0800

    1

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Guofeng Luo on 2026-06-14.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
